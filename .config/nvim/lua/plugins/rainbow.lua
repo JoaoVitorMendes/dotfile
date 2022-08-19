5371718f2fd60a655d1d6bc4f93fc6413f31383d
João Vitor <60587000+JoaoVitorMendes@users.noreply.github.com>
2022-08-19 16:53:55 -0300

    Create rainbow.lua

--- .config/nvim/lua/plugins/rainbow.lua
+++ .config/nvim/lua/plugins/rainbow.lua
@@ -3,4 +3,4 @@
 vim.cmd([[ let g:rainbow_active = 1 ]])
 
 vim.cmd([[ let g:rainbow_guifgs = ['#8C2C23', '#613923', '#A6583C', '#BFAC95'] ]])
-vim.cmd([[ let g:rainbow_ctermfgs = ['red', 'lightgreen', 'yellow', 'red', 'magenta'] ]]
+vim.cmd([[ let g:rainbow_ctermfgs = ['red', 'lightgreen', 'yellow', 'red', 'magenta'] ]])
