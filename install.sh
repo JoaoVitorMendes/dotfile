51d4443ee50b9014b36f37bc24537d661a47b595
João Vitor <joaovitorm@protonmail.ch>
2023-01-01 16:59:32 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
 yay -S gdb ninja gcc cmake libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland
+
+
+git clone --recursive https://github.com/hyprwm/Hyprland
+cd Hyprland
+sudo make install
